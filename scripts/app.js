5bf49cd552499fab7917f4119e545c24250cc275
EMILYHLANE <embem80@gmail.com>
2018-09-27 15:23:00 -0700

    add small screen closed nav styles and js psudocode

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
 //document ready
 
-//hide nav close --> add id "hide-close"
+//hide nav close (X) --> add id "close-hide" to element with class = "fa-times"
+//show nav for small screens --> add class "nav-small" to nav element or class = "nav-container"
+//#hamburger-hide --> add to class=fa-bars
