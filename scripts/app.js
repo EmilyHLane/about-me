3d014d03355d18d0fdbbc9e594a19480a1486e07
EMILYHLANE <embem80@gmail.com>
2018-09-27 17:03:37 -0700

    adds layout styles and placeholder image

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,8 @@
 //hide nav close (X) --> add id "close-hide" to element with class = "fa-times"
 //show nav for small screens --> add class "nav-small" to nav element or class = "nav-container"
 //#hamburger-hide --> add to class=fa-bars
+//slide nav up/down
+//add id "test-hide" to class testimonial-1, -2, -3 to hide
+//fade testimonials in/out
+
+//is there a better way to show/hide than use id to hide?
