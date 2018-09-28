33be443fba3305fa78b9b9dcb4e20f067c62970c
EMILYHLANE <embem80@gmail.com>
2018-09-28 15:51:02 -0700

    adds functional small screen menu, adds large screen nav bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,36 @@
-//document ready
+//document ready for jQuery
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
 
-//hide nav close (X) --> add id "close-hide" to element with class = "fa-times"
-//show nav for small screens --> add class "nav-small" to nav element or class = "nav-container"
-//#hamburger-hide --> add to class=fa-bars
-//slide nav up/down
+//show nav on hamburger click
+$(".fa-bars").on("click", showNav);
+
+function showNav() {
+	//show nav for small screens 
+	$(".nav-links").toggleClass("nav-small");
+    $(".nav-links").toggleClass("nav-hide");
+    $(".fa-times").toggleClass("close-show");
+    $(".fa-times").attr("id", " ");
+	$(".fa-bars").toggle("#hamburger-hide");
+	//slide nav down
+}
+
+//close nav on X click
+$(".fa-times").on("click", closeNav);
+
+function closeNav() {
+	$(".nav-links").toggleClass("nav-small");
+    $(".nav-links").toggleClass("nav-hide");
+    $(".fa-times").toggleClass("close-show");
+    $(".fa-times").attr("id", "close-hide");
+    $(".fa-bars").toggle("#hamburger-hide");
+    //slide nav up
+}
+
+
+	
+
 //add id "test-hide" to class testimonial-1, -2, -3 to hide
 //fade testimonials in/out
 
